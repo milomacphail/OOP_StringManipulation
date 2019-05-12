7093844d2454ddb4e4a20c1b79bbf39eaad5791d
milomacphail <milomacphail@gmail.com>
2019-05-11 19:30:27 -0600

    Added text summarization algorithm

---
+++ Summarizing_Text/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Summarizing_Text
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string sentence = "A really really really really really really really really long sentence.";
+            var summary = SummarizeText(sentence, 25);
+            Console.WriteLine(summary);
+        }
+
+        static string SummarizeText(string text, int maxLength =20)
+        {
+            if (text.Length < maxLength)
+                Console.WriteLine(text);
+
+            var words = text.Split();
+                    int totalChar = 0;
+                    var summary = new List<string>();
+
+                    foreach (string word in words)
+                    {
+                        summary.Add(word);
+
+                        totalChar += word.Length + 1;
+                        if (totalChar > maxLength)
+                            break;
+                    }
+
+            return string.Join(" ", summary) + "...";
+
+            }
+        }
+    }
+}
